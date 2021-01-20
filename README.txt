329b6535f86fa86a7b089d08758fa97690f505ba
Leslie Krause <rkrause@inbox.com>
2021-01-19 19:00:27 -0500

    Build 03 - implemented globaltimer as singleton object - refactored Timekeeper class - added sanity check to start method

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-Timekeeper Mod v1.1
+Timekeeper Mod v1.2
 By Leslie E. Krause
 
 Timekeeper acts as a centralized dispatcher for all time-sensitive routines, thereby 
@@ -27,7 +27,7 @@ License of source code
 
 The MIT License (MIT)
 
-Copyright (c) 2020, Leslie Krause (leslie@searstower.org)
+Copyright (c) 2020-2021, Leslie Krause (leslie@searstower.org)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
 software and associated documentation files (the "Software"), to deal in the Software
--- init.lua
+++ init.lua
@@ -7,64 +7,95 @@
 -- ./games/just_test_tribute/mods/timekeeper/init.lua
 --------------------------------------------------------
 
+local g_timer_list = { }
+local g_clock = 0.0
+
+globaltimer = { }
+
+globaltimer.start = function ( period, name, func, delay )
+	assert( string.find( name, minetest.get_current_modname( ) .. ":" ) == 1 )
+
+	table.insert( g_timer_list, {
+		cycles = 0,
+		period = period,
+		expiry = g_clock + period + ( delay or 0.0 ),
+		func = func,
+	} )
+end
+
+minetest.register_globalstep( function ( dtime )
+	g_clock = g_clock + dtime
+
+	for i, v in ipairs( g_timer_list ) do
+		if g_clock >= v.expiry or g_clock == 0.0 then
+			v.expiry = g_clock + v.period
+			-- callback( cycles, period, uptime, overrun )
+			v.func( v.cycles, v.period, g_clock, g_clock - v.expiry )
+			v.cycles = v.cycles + 1
+		end
+	end
+end )
+
+minetest.after( 0.0, function ( )
+	globaltimer.start = nil
+end )
+
 function Timekeeper( this )
-	local timer_defs = { }
-	local pending_timer_defs = { }
+	local timers = { }
+	local pending_timers = { }
 	local clock = 0.0
 	local self = { }
 
 	self.start = function ( period, name, func, delay )
-		if timer_defs[ name ] then
-			timer_defs[ name ] = nil
+		assert( globaltimer.start == nil )  -- only start timers once environment is initialized
+
+		if timers[ name ] then
+			timers[ name ] = nil
 		end
-		pending_timer_defs[ name ] = { cycles = 0, period = period, expiry = clock + period + ( delay or 0.0 ), started = clock, func = func }
+		pending_timers[ name ] = { cycles = 0, period = period, expiry = clock + period + ( delay or 0.0 ), started = clock, func = func }
 	end
 
 	self.start_now = function ( period, name, func )
-		if timer_defs[ name ] then
-			timer_defs[ name ] = nil
+		assert( globaltimer.start == nil )  -- only start timers once environment is initialized
+
+		if timers[ name ] then
+			timers[ name ] = nil
 		end
 		if not func( this, 0, period, 0.0, 0.0 ) then
-			pending_timer_defs[ name ] = { cycles = 0, period = period, expiry = clock + period, started = clock, func = func }
+			pending_timers[ name ] = { cycles = 0, period = period, expiry = clock + period, started = clock, func = func }
 		end
 	end
 
 	self.clear = function ( name )
-		pending_timer_defs[ name ] = nil
-		if timer_defs[ name ] then
-			timer_defs[ name ] = nil
+		pending_timers[ name ] = nil
+		if timers[ name ] then
+			timers[ name ] = nil
 		end
 	end
 
 	self.on_step = function ( dtime )
 		clock = clock + dtime
 
-		for k, v in pairs( pending_timer_defs ) do
-			timer_defs[ k ] = v
-			pending_timer_defs[ k ] = nil
+		for k, v in pairs( pending_timers ) do
+			timers[ k ] = v
+			pending_timers[ k ] = nil
 		end
 
-		local timers = { }
-		for k, v in pairs( timer_defs ) do
-			if clock >= v.expiry and clock > v.started then
+		local running_timers = { }
+		for k, v in pairs( timers ) do
+			if clock >= v.expiry then
 				v.expiry = clock + v.period
 				v.cycles = v.cycles + 1
 				-- callback( this, cycles, period, elapsed, overrun )
 				if v.func and v.func( this, v.cycles, v.period, clock - v.started, clock - v.expiry ) then
 					self.clear( k )
 				end
-				timers[ k ] = v
+				running_timers[ k ] = v
 			end
 		end
 
-		return timers
+		return running_timers
 	end
 
 	return self
 end
-
-globaltimer = Timekeeper( { } )
-
-minetest.register_globalstep( function ( dtime )
-	globaltimer.on_step( dtime )
-end )
